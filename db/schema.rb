6ce041fd504f85600f4acb6a4611783824017b9d
Jake Burton <jburto94@yahoo.com>
2022-06-15 22:31:05 -0400

    linked users and tweets

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_16_005551) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_16_014047) do
   create_table "tweets", force: :cascade do |t|
     t.text "tweet"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
